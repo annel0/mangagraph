342d28eea935e98994b1221abe805721f91e310e
damirTAG <damirtagilbayev17@gmail.com>
2025-03-13 04:01:44 +0500

    fix setup

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from setuptools import setup, find_packages
 
+import pathlib
+
 
 LIB_NAME = 'mangagraph'
+
+WORK_DIR = pathlib.Path(__file__).parent
 
 VERSION = '0.0.3'
 
@@ -9,7 +13,7 @@ setup(
     name=LIB_NAME,
     version=VERSION,
     description='Async manga parser-converter from mangalib to telegraph pages',
-    long_description=open('readme.md', encoding='utf-8').read(),
+    long_description = (WORK_DIR / "README.md").read_text("utf-8"),
     long_description_content_type='text/markdown',
     url='https://github.com/damirTAG/mangagraph',
     author='damirTAG',
